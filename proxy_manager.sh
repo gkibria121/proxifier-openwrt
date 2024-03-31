1366fe43fa314da240e39272443ed55439e4b4a9
Ahmed Kibria <gkibria121@gmail.com>
2024-03-31 22:55:59 +0600

    manage redsocks config from client

--- proxy_manager.sh
+++ proxy_manager.sh
@@ -2,6 +2,11 @@
 
 PROXY_LIST="proxy_list.txt"
 CONFIG_FILE="redsocks.conf"
+HOST=192.168.1.1
+USER="root"
+PASS="gksumu121"
+PORT=22
+
 
 # Function to set proxy configuration in redsocks.conf
 function set_proxy_config {
@@ -13,12 +18,14 @@ function set_proxy_config {
     local config_file=$6
 
     # Update the redsocks configuration block in the config file
-
+    process_server
     sed -i "s|^\s*ip\s*.*$|        ip = $ip;|" "$config_file"
     sed -i "s|^\s*port\s*.*$|        port = $port;|" "$config_file"
     sed -i "s|^\s*type\s*.*$|        type = $type;|" "$config_file"
     sed -i "s|^\s*login\s*.*$|        login = \"$username\";|" "$config_file"
     sed -i "s|^\s*password\s*.*$|        password = \"$password\";|" "$config_file"
+    update_server
+
 }
 
 function process_set_proxy_config {
@@ -75,18 +82,44 @@ function remove_proxy {
         echo "Proxy not found in the list."
     fi
 }
+function process_server {
+    echo "Downloading"
+    pscp -scp  -pw $PASS -P $PORT $USER@$HOST:/etc/redsocks.conf $CONFIG_FILE
+    echo "done"
 
+
+}
+function update_server {
+    echo "Uploading"
+    pscp -scp -pw "$PASS" -P "$PORT" $CONFIG_FILE "$USER@$HOST:/etc/redsocks.conf"
+
+
+    plink -ssh -l $USER -pw $PASS -no-antispoof $HOST -P $PORT "service redsocks start"
+
+    plink  -ssh -l $USER -pw $PASS $HOST -no-antispoof  -P $PORT service redsocks restart
+    echo "done"
+
+}
+function show_current_ip {
+
+   current_ip=$(curl -s ifconfig.me)
+   echo "My public IP address is: $current_ip"
+
+}
 echo "Proxy Manager"
 
 while true; do
+    echo "0. Show Current IP"
     echo "1. Show Proxies"
     echo "2. Add Proxy"
     echo "3. Remove Proxy"
     echo "4. Set Proxy Configuration"
     echo "5. Quit"
     read -p "Enter your choice: " choice
+    clear
 
     case $choice in
+        0) show_current_ip ;;
         1) show_proxies ;;
         2) add_proxy ;;
         3) remove_proxy ;;
@@ -94,4 +127,5 @@ while true; do
         5) echo "Exiting."; break ;;
         *) echo "Invalid choice. Please select again." ;;
     esac
+    
 done
